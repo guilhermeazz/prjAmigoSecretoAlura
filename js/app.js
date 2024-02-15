e8664635c789510633e5646eceb7b6d0591e1eec
Guilherme Albuquerque <guialbuzapa@gmail.com>
2024-02-15 14:27:14 -0300

    embaralhar, limite para sortear

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
-let incluidos = document.getElementById('lista-amigos');
-let listaDeAmigos = [];
+let amigos = [];
+document.getElementById('adicionar').setAttribute('disabled', true);
 
 function adicionar(){
     //Receber os nomes
     //adicionar os nomes a lista de amigos 
     let nomes = document.getElementById('nome-amigo').value;
+    let incluidos = document.getElementById('lista-amigos');
     console.log(nomes);
 
-    listaDeAmigos.push(" " + nomes);
-    console.log(listaDeAmigos);
 
-    incluidos.innerHTML = listaDeAmigos;
+
+    if(amigos.includes(nomes)){
+        alert("Ops! Parece que foram adicionados nomes iguais. Você pode optar por sobrenomes ou um apelido carinhoso para a diferenciação. ;)")
+    }else if(nomes.trim() == ""){
+        alert("Ops! Nenhum nome foi identificado para ser adicionado. Tente novamente.")
+    }else{
+        amigos.push(nomes);
+
+        if (incluidos.textContent == ""){
+            incluidos.textContent = nomes;
+        }else{
+            incluidos.textContent = incluidos.textContent + ', ' + nomes;
+        }
+    
+    }
 
     //limpar campo para escrever os nomes 
     document.getElementById('nome-amigo').value = "";
@@ -19,13 +32,46 @@ function adicionar(){
 function sortear(){
     //juntar os nomes por sorteios 
     //Evitar repetidos e sortar a si mesmo
+    document.getElementById('lista-sorteio').innerHTML = "";
     
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    if (amigos.length < 4) {
+        alert("É necessario pelo menos 4 pessoas para fazer um sorteio");
+        return;
+    }
+    
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+            }
+    }
+
+    
+}
+function reiniciar(){
+    //Apagar a lista de amigos 
+    amigos = [];
+    document.getElementById('lista-amigos').textContent = "";
+    document.getElementById('lista-sorteio').innerHTML = "";
     
 }
 
-function reiniciar(){
-    //Apagar a lista de amigos 
-    document.getElementById('lista-amigos').textContent = "";
-    listaDeAmigos = [];
-    
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function ativarBotao(){
+
 }
